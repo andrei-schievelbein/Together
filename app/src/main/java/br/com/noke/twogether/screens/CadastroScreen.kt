8a9947feb9c55e99c5ccefb76606672f00969462
Andrei Schievelvein <andreicel@gmail.com>
2024-09-02 17:43:12 -0300

    together.gif

--- app/src/main/java/br/com/noke/twogether/screens/CadastroScreen.kt
+++ app/src/main/java/br/com/noke/twogether/screens/CadastroScreen.kt
@@ -1,7 +1,6 @@
 package br.com.noke.twogether.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
